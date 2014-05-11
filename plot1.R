9a35e8d1e636e8d2e3aa6c143e858fd198477197
prusswan <prusswan@gmail.com>
2014-05-12 06:11:53 +0800

    added plot1

---
+++ plot1.R
@@ -0,0 +1,12 @@
+cols = c('character', 'character', 'numeric', 'numeric', 'numeric', 'numeric','numeric', 'numeric', 'numeric')
+data <- read.table(unz("data.zip", "household_power_consumption.txt"), header=T, sep=";", stringsAsFactors=F, colClasses=cols, na.strings='?')
+library(data.table)
+data <- data.table(data)
+
+data <- data[Date %in% c("1/2/2007","2/2/2007")]
+
+png("plot1.png", height=480, width=480)
+
+hist(data$Global_active_power, col='red', xlab = 'Global Active Power (kilowatts)', main = 'Global Active Power')
+
+dev.off()
